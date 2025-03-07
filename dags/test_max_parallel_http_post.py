cb07cbd265ddaeeb2516d795af880243e8abc1d7
aamir-syed <aamir.a.syed@gmail.com>
2025-03-07 23:34:55 +0300

    Update test_max_parallel_http_post.py

--- dags/test_max_parallel_http_post.py
+++ dags/test_max_parallel_http_post.py
@@ -16,7 +16,6 @@ with DAG(
     start_date=days_ago(1),
     catchup=False,
     max_active_tasks=25,  # Max parallel tasks allowed
-    max_active_tis_per_dag=5,  # Limits max parallel running task instances
 ) as dag:
     
     # Create multiple parallel HTTP tasks
