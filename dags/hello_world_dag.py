0985500d79b401fe7281f09b83988bbc6b7b6ee9
aamir-syed <aamir.a.syed@gmail.com>
2025-03-09 22:41:59 +0300

    Update hello_world_dag.py

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -3,7 +3,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 def helloWorld():
-    print('Hello World 2')
+    print('Hello World  - Wedo')
 
 with DAG(dag_id="hello_world_dag",
          start_date=datetime(2021,1,1),
